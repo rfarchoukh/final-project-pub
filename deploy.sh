87041f143393979f1221f51c158d44dfa10725cf
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-12-17 21:51:33 -0500

    Added check to see what branch we're running.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 
 echo "Hello world!"
 
-echo "The current branch is ${BRANCH_NAME}"
+if [ "${BRANCH_NAME}" == "dev" ]; then
+	echo "The current branch is ${BRANCH_NAME}"
+elif [ "${BRANCH_NAME}" == "stage" ]; then
+	echo "The current branch is ${BRANCH_NAME}"
+elif [ "${BRANCH_NAME}" == "master" ]; then
+        echo "The current branch is ${BRANCH_NAME}"
+else
+	echo "Invalid branch, SOMEHOW."
+fi
 
 exit 0
