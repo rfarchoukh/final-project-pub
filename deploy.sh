780684c3814b661fd52ed422c3c9746b60f9b009
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-12-17 21:23:38 -0500

    Changing path of gcloud

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "Hello world!"
 echo ""
 echo "Testing gcloud..."
-/home/centos/google-cloud-sdk/bin/gcloud compute instances describe ansible-server --zone us-east1-b
+/google-cloud-sdk/bin/gcloud compute instances describe ansible-server --zone us-east1-b
 
 exit 0
