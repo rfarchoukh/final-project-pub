3a7b12d74ce9bd6cfaabaa65be16d97b3564a293
Gabriel Sanna <gabrielsanna1@gmail.com>
2018-12-17 21:19:37 -0500

    Adding test gcloud command

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "Hello world!"
 echo ""
 echo "Testing gcloud..."
-gcloud compute instances describe ansible-server --zone us-east1-b
+/home/centos/google-cloud-sdk/bin/gcloud compute instances describe ansible-server --zone us-east1-b
 
 exit 0
