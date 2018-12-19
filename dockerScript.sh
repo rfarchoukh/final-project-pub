d7e1e996812f9c8c789ef913035ee5a9b239aee2
Scott Ouellette <scottx611x@gmail.com>
2018-12-18 22:20:35 -0500

    Set http server port based off of current git branch

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 docker build . -t http_server
-docker run -d -p 8888:80 http_server
+
+if [ "${BRANCH_NAME}" = "dev" ] || [ "${BRANCH_NAME}" = "stage" ]; then
+    HTTP_SERVER_PORT=8888
+else
+    HTTP_SERVER_PORT=80
+fi
+
+docker run -d -p $HTTP_SERVER_PORT:80 http_server
